623b950dcb88f14dda6131480a5f7587a978267d
Oz <ozcodx@gmail.com>
2025-01-21 13:09:50 -0500

    Update homepage URL, refine Vite configuration for build output, and change deployment branch to master

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true
+  },
   css: {
     preprocessorOptions: {
       scss: {
