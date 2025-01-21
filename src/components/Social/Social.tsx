003d3a9f70df79628678951aaf12a55f55943906
Oz <ozcodx@gmail.com>
2025-01-21 14:59:47 -0500

    Update Social component

--- src/components/Social/Social.tsx
+++ src/components/Social/Social.tsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import { FaGithub, FaLinkedin, FaInstagram, FaEnvelope } from 'react-icons/fa'
 import './Social.scss'
 
 export const Social = () => {
@@ -6,14 +6,17 @@ export const Social = () => {
     <section id="social" className="social">
       <h2>CONNECT WITH ME<br />SOCIAL MEDIA</h2>
       <div className="social-links">
+        <a href="mailto:ozcodx@gmail.com" aria-label="Email">
+          <FaEnvelope size={32} />
+        </a>
         <a href="https://github.com/ozcodx" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
           <FaGithub size={32} />
         </a>
         <a href="https://linkedin.com/in/ozcodx" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
           <FaLinkedin size={32} />
         </a>
-        <a href="https://twitter.com/ozcodx" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-          <FaTwitter size={32} />
+        <a href="https://www.instagram.com/ozcodx/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+          <FaInstagram size={32} />
         </a>
       </div>
     </section>
