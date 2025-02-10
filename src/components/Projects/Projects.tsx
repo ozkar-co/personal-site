e4d78a82d803c968e1b920bfa4c01a5a7b0f3cd5
Oz <ozcodx@gmail.com>
2025-02-10 13:05:28 -0500

    Add project image and update Projects component with Oz RO project details

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,31 +1,21 @@
 import './Projects.scss';
+import placeholderImage from '../../assets/placeholder.png';
+import project1 from '../../assets/proy1.png';
+
 
 interface Project {
   title: string;
   description: string;
   link: string;
+  image: string;
 }
 
 const projects: Project[] = [
   {
-    title: '',
-    description: 'Descripción del Proyecto 1.',
-    link: 'https://ejemplo1.com',
-  },
-  {
-    title: 'Proyecto 2',
-    description: 'Descripción del Proyecto 2.',
-    link: 'https://ejemplo2.com',
-  },
-  {
-    title: 'Proyecto 3',
-    description: 'Descripción del Proyecto 3.',
-    link: 'https://ejemplo3.com',
-  },
-  {
-    title: 'Proyecto 4',
-    description: 'Descripción del Proyecto 4.',
-    link: 'https://ejemplo4.com',
+    title: 'Oz RO',
+    description:'Servidor privado de Ragnarok Online',
+    link: 'https://oz-ragnarok.web.app/',
+    image: project1,
   },
   // Agrega más proyectos según sea necesario
 ];
@@ -40,6 +30,12 @@ export const Projects = () => {
         <div className="projects-grid">
             {projects.map((project, index) => (
                 <div key={index} className="project-item">
+                <img 
+                  src={project.image} 
+                  alt={project.title} 
+                  className="project-image" 
+                  onError={(e) => { e.currentTarget.src = placeholderImage; }}
+                />
                 <h2>{project.title}</h2>
                 <p>{project.description}</p>
                 <a href={project.link} target="_blank" rel="noopener noreferrer">Ver Proyecto</a>
